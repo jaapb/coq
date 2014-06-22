8cc344b0f18ac5f0e359348943b4d8f840e99a58
Jason Gross <jgross@mit.edu>
2014-06-22 18:09:09 -0400

    More test-suite cases

--- test-suite/bugs/closed/3300.v
+++ test-suite/bugs/closed/3300.v
@@ -1,7 +1,10 @@
 Set Primitive Projections.
-Record Box (T : Type) : Prop := wrap {prop : T}.
+Require Import Coq.Init.Specif Coq.Init.Datatypes Coq.Init.Logic.
 
-Definition down (x : Type) : Prop := Box x.
-Definition up (x : Prop) : Type := x.
+Parameters A B : Prop.
+Parameter P : A -> Prop.
 
-Fail Definition back A : up (down A) -> A := @prop A.
+Fail Check fun x : sigT P => (eq_refl : x = existT P (projT1 x) (projT2 x)).
+Fail Check fun x : sig P => (eq_refl : x = exist P (proj1_sig x) (proj2_sig x)).
+Fail Check fun x : A * B => (eq_refl : x = (fst x, snd x)).
+Fail Check fun x : A /\ B => (eq_refl : x = conj (proj1 x) (proj2 x)).
